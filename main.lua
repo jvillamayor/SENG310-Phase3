df44bcc7b4925307c0d59881a468cb667df2d341
Julius <juliusvillamayor@gmail.com>
2017-03-06 11:29:10 -0800

    New Assets Folder
    - moved images into the assets folder
    - cleaned up code

--- main.lua
+++ main.lua
@@ -1,15 +1,13 @@
 -----------------------------------------------------------------------------------------
 --
 -- main.lua
+-- Cara Pekson, Aman Bhayani, Julius Villamayor
 --
 -----------------------------------------------------------------------------------------
 
--- Your code here
-
-
 local widget = require("widget")
 
-local logo = display.newImage("logo.png")
+local logo = display.newImage("assets/logo.png")
 logo.x = 160; logo.y = 220
 
 local myTextObject = display.newText("Recipe Finder", 160, 240, "Avenir",45)
@@ -18,32 +16,13 @@ myTextObject:setFillColor(0,0,0)
 display.setDefault( "background", 1, 1, 1 )
 myTextObject.y = 50
 
-
 local username = native.newTextField(150, 370, 220, 36)
-
---[[	top = 150,
-	left = 20,
-	width = 220,
-	height = 36,
-	cornerRadius = 8,
-	strokeWidth = 3, 
-	strokeColor = {0,0,0},
-	font = "Avenir",
-	fontSize = 20
-}
-
-)
-]]--
-
-
-	---(150, 370, 220, 36)
 
 username.inputType = "username"
 username.placeholder = "username"
 local password = native.newTextField(150, 430, 220, 36)
 password.inputType = "password"
 password.placeholder = "password"
-
 
 local signin = widget.newButton(
 {
@@ -59,24 +38,8 @@ local signin = widget.newButton(
 	fillColor = { default = {165,198,209,0}, over= {165,198,209,0} },
 	strokeColor = { default = {1,1,1,1}, over= {1, 0.1,0.7,0.4} },
 	strokeWidth = 4
-
-
-
-
 }
 	)
 
 signin.y = 480
 signin.x = 160
---[[local mytextfield = widget.newTextField (
-	{
-	top = 10,
-	left = 20,
-	width = 200,
-	height = 30,
-	cornerRadius = 8,
-	strokeWidth = 3,
-	strokeColor = {0,0,0},
-	}
-	)
---]]
