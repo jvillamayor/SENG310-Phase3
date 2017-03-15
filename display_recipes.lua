ed8d70dc368b18f828d37c903c224253774b82bc
cpekson <cara@w134-87-157-125.wireless.uvic.ca>
2017-03-15 13:37:18 -0700

    new page display_recipes draft,

--- display_recipes.lua
+++ display_recipes.lua
@@ -1,6 +1,6 @@
 -----------------------------------------------------------------------------------------
 --
--- profile.lua
+-- display_recipes.lua
 -- Cara Pekson, Aman Bhayani, Julius Villamayor
 --
 -----------------------------------------------------------------------------------------
@@ -10,11 +10,6 @@ local widget = require("widget")
 local dropdown = require('dropdown')
 local screen = require('screen')
 local scene = composer.newScene()
-
-
-local function changeScenes()
-    composer.gotoScene("display_recipes", {effect="slideLeft", time=500})
-end
 
 local function delayedSceneRemoval()
     local function removeSceneListener(event)
@@ -32,130 +27,40 @@ function scene:create( event )
     myTextObject.y = 5
     sceneGroup:insert(myTextObject)
 
-    local title = display.newText("Choose Category", 160, 240, "Avenir", 18)
+    local title = display.newText("Chicken Soup", 160, 240, "Avenir", 18)
     title:setFillColor(0,0,0)
-    title.x = 100
+    title.x = 160
     title.y = 70
     sceneGroup:insert(title)
 
-    local ethnicity = display.newText("Ethnicity", 160, 240, "Avenir", 18)
+    local ethnicity = display.newText("Tomato Soup", 160, 240, "Avenir", 18)
     ethnicity:setFillColor(0,0,0)
-    ethnicity.x = 77
-    ethnicity.y = 120
+    ethnicity.x = 160
+    ethnicity.y = 100
     sceneGroup:insert(ethnicity)
 
-    local ethnicity_ex = display.newText("Asian", 160, 240, "Avenir", 15)
+    local ethnicity_ex = display.newText("Eggs and Toast", 160, 240, "Avenir", 18)
     ethnicity_ex:setFillColor(0,0,0)
-    ethnicity_ex.x = 200
-    ethnicity_ex.y = 120
+    ethnicity_ex.x = 160
+    ethnicity_ex.y = 150
     sceneGroup:insert(ethnicity_ex)
 
 
-    local calories = display.newText("Calories", 160, 240, "Avenir", 18)
+    local calories = display.newText("Grilled Cheese", 160, 240, "Avenir", 18)
     calories:setFillColor(0,0,0)
-    calories.x = 75
+    calories.x = 160
     calories.y = 200
     sceneGroup:insert(calories)
 
-    local calories_ex = display.newText("210kCal", 160, 240, "Avenir", 15)
+    local calories_ex = display.newText("Miso Soup", 160, 240, "Avenir", 18)
     calories_ex:setFillColor(0,0,0)
-    calories_ex.x = 200
-    calories_ex.y = 200
+    calories_ex.x = 160
+    calories_ex.y = 240
     sceneGroup:insert(calories_ex)
 
-    local budget = display.newText("Budget", 160, 240, "Avenir", 18)
-    budget:setFillColor(0,0,0)
-    budget.x = 75
-    budget.y = 280
-    sceneGroup:insert(budget)
 
-
-    local budget_ex = display.newText("<= $50", 160, 240, "Avenir", 15)
-    budget_ex:setFillColor(0,0,0)
-    budget_ex.x = 200
-    budget_ex.y = 280
-    sceneGroup:insert(budget_ex)
-
-    local time = display.newText("Time", 160, 240, "Avenir", 18)
-    time:setFillColor(0,0,0)
-    time.x = 67
-    time.y = 360
-    sceneGroup:insert(time)
-
-
-    local time_ex = display.newText("15 mins", 160, 240, "Avenir", 15)
-    time_ex:setFillColor(0,0,0)
-    time_ex.x = 200
-    time_ex.y = 360
-    sceneGroup:insert(time_ex)
-
-
----    local update = display.newText("Search Recipes", 160, 240, "Avenir", 18)
---- update:setFillColor(0,0,0)
----    update.x = 230
- ---   update.y = 450
-    ----sceneGroup:insert(update)
- 
-
-    local update = widget.newButton(
-    {
-        left = 120,
-        right = 200,
-        id = "update",
-        label = "Search Recipes",
-        onEvent = handleButtonEvent,
-        width = 200,
-        font = "Avenir",
-        height = 40
-     ---   fillColor = { default = {165,198,209,0}, over= {165,198,209,0} },
-      ---  strokeWidth = 4
-    }
-        )
-
-    update.y = 450
-    update.x = 230
-
-
-
--- ScrollView listener
-    local function scrollListener( event )
      
-        local phase = event.phas
-        return true
-    end
-     local scrollBarOpt = {
-        width = 20,
-        height = 20,
-        numFrames = 3,
-        sheetContentWidth = 20,
-        sheetContentHeight = 60
-    }
-    -- Create the widget
-    local scrollView = widget.newScrollView(
-        {
-            y = 238,
-            x = 170,
-            width = 230,
-            height = 25,
-            scrollWidth = 250,
-            scrollHeight = 25,
-            listener = scrollListener,
-            hideBackground = true,
-            verticalScrollDisabled = true,
-        }
-    )
 
-    scrollView:scrollToPosition {
-      x = 100
-    }
-     
-    -- Create a image and insert it into the scroll view
-    local background = display.newImageRect( "assets/scroll_button.png", 30, 30 )
-    background.y = 10
-    background.x = 8
-    scrollView:insert( background )
-
-    sceneGroup:insert(scrollView)
     --- DROPDOWN MENU
       local myDropdown
 
@@ -237,9 +142,6 @@ function scene:create( event )
       }
       sceneGroup:insert(myDropdown)
 
-
-    sceneGroup:insert(update)
-    update:addEventListener("tap", changeScenes)
     end
 
 -- show()
